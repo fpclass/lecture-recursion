e5f30c87d81db9b2404d5f68c7a0db3ffee44de4
Michael Gale <m.gale@warwick.ac.uk>
2019-01-15 20:13:38 +0000

    Add examples for local functions

---
+++ src/LocalFunctions.hs
@@ -0,0 +1,21 @@
+--------------------------------------------------------------------------------
+-- Functional Programming (CS141)                                             --
+-- Lecture 6: Recursive functions                                                    --
+--------------------------------------------------------------------------------
+
+module LocalFunctions where
+
+--------------------------------------------------------------------------------
+
+fac :: (Eq a, Num a) => a -> a
+fac x = go x 1
+    where go 0 m = m
+          go n m = go (n-1) (n*m)
+
+intercalate :: String -> [String] -> String
+intercalate sep xs = go xs
+    where go []     = ""
+          go [x]    = x
+          go (x:xs) = x ++ sep ++ go xs
+
+--------------------------------------------------------------------------------
